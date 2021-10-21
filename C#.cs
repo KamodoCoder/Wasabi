ba7dd542171073902c0d8fb02270de0e831ea340
KamodoCoder <92625972+KamodoCoder@users.noreply.github.com>
2021-10-21 09:25:35 +0100

    Update C#.cs

--- C#.cs
+++ C#.cs
@@ -9,3 +9,7 @@ namespace wasabiName
     }
   }
 }
+
+/* Above is the standard model for a C# programme. We have our namespace (wasabiName), which is a collection of classes, currently only one (wasabiClass)
+   which is a collection of functions (methods) and definitions. Currently we only have one function, which is the Main function: a default function 
+   always run in the programme, and hence has to be there */
