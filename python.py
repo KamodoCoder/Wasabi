ad1926603a585e77b35059590754708711846c24
KamodoCoder <92625972+KamodoCoder@users.noreply.github.com>
2021-10-21 09:10:38 +0100

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+import sys
+import os 
+import math
+import string
+import random as rd
+import itertools as it
+
+# Above, I have pre-imported the following modules: sys, os, math, string, random, itertools. These are used for terminal input, file navigation, ...
+# ...mathematical operations (of higher difficulty), alphabetical dealings, random choosing, and iterable manipulation respectfully.
+
+# Create a new branch, whatever you want!
