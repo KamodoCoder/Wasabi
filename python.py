77176a402605d98eaf0d5f854a9ad0969106ea2b
KamodoCoder <92625972+KamodoCoder@users.noreply.github.com>
2022-01-09 18:20:04 +0000

    Update python.py

--- python.py
+++ python.py
@@ -5,6 +5,9 @@ import string
 import random as rd
 import itertools as it
 
+
+print("This is the only alteration to the python skeleton of this branch")
+
 # Above, I have pre-imported the following modules: sys, os, math, string, random, itertools. These are used for terminal input, file navigation, ...
 # ...mathematical operations (of higher difficulty), alphabetical dealings, random choosing, and iterable manipulation respectfully.
 
